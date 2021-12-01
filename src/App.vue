da4817cd1ada7806d5394da89b1d356da2e8d593
Alexey <alex967268@gmail.com>
2021-12-01 22:57:23 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="app">
+        <Sidebar />
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Sidebar from "@/components/Sidebar"
+import "@/assets/style.scss"
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+    components: {
+        Sidebar,
+    },
+    data() {
+        return {
+            posts: [],
+        }
+    },
+}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.app {
+    color: var(--text);
 }
 </style>
