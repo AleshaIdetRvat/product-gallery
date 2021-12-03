f98ef662d50d5d40ed639aa851d990a2c948062d
Alexey <alex967268@gmail.com>
2021-12-03 20:50:25 +0300

    add component `MainSelector` to the UI-lib

---
+++ src/components/UI/MainSelector.vue
@@ -0,0 +1,42 @@
+<template>
+    <select class="select" :value="modelValue" @change="changeHandler">
+        <option class="select__option" disabled selected value="">
+            Выберите из списка
+        </option>
+
+        <option v-for="item in options" :key="item.value" :value="item.value">
+            {{ item.name }}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: "main-select",
+    props: {
+        modelValue: {
+            type: String,
+        },
+        options: {
+            type: Array,
+            default: () => [],
+        },
+    },
+    methods: {
+        changeHandler(event) {
+            this.$emit("update:modelValue", event.target.value)
+        },
+    },
+}
+</script>
+
+<style scoped>
+.select {
+    cursor: pointer;
+    padding: 8px;
+    border-radius: 8px;
+    background: var(--white);
+}
+.select__option {
+}
+</style>
